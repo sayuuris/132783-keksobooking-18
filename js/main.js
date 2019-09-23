6d0f9f72a5ec4b56f96cbc4af42fce46165ae902
sayuris <oleq.artyomov@gmail.com>
2019-09-23 19:24:34 +0400

    исправляет значения в adres

--- js/main.js
+++ js/main.js
@@ -6,13 +6,19 @@ var OFFER_TYPE = ['palace', 'flat', 'house', 'bungalo'];
 var OFFER_ROOMS = [1, 2, 3, 4];
 var OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var OFFER_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var AVATAR_WIDTH = 50;
+var AVATAR_HEIGHT = 70;
+var LOCATION_X_MIN = 40;
+var LOCATION_X_MAX = 1220;
+var LOCATION_Y_MIN = 0;
+var LOCATION_Y_MAX = 730;
 
 var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-var getRandomArrFromParent = function (parent) {
-  return parent.filter(function () {
+var getRandomArrFromParent = function (parentArray) {
+  return parentArray.filter(function () {
     return getRandomInt(0, 1);
   });
 };
@@ -21,16 +27,21 @@ var activateElem = function (elem, className) {
   elem.classList.remove(className);
 };
 
+
 var getOffers = function () {
   var result = [];
+  var randomLocationX = 0;
+  var randomLocationY = 0;
   for (var i = 1; i <= OFFER_AMOUNT; i++) {
+    randomLocationX = getRandomInt(LOCATION_X_MIN, LOCATION_X_MAX);
+    randomLocationY = getRandomInt(LOCATION_Y_MIN, LOCATION_Y_MAX);
     result.push({
       author: {
         'avatar': 'img/avatars/user0' + i + '.png'
       },
       offer: {
         title: 'заголовок предложения',
-        address: '600, 350',
+        address: randomLocationX + ',' + randomLocationY,
         price: getRandomInt(5000, 100000),
         type: OFFER_TYPE[getRandomInt(0, 3)],
         rooms: OFFER_ROOMS[getRandomInt(0, 3)],
@@ -42,13 +53,14 @@ var getOffers = function () {
         photos: getRandomArrFromParent(OFFER_PHOTOS)
       },
       location: {
-        x: getRandomInt(10, 90),
-        y: getRandomInt(130, 630)
+        x: randomLocationX,
+        y: randomLocationY
       }
     });
   }
   return result;
 };
+
 var mapElem = document.querySelector('.map');
 activateElem(mapElem, 'map--faded');
 
@@ -58,15 +70,13 @@ var pinTemplate = document.querySelector('#pin')
 
 var renderPinFromTemplate = function (data) {
   var pinElem = pinTemplate.cloneNode(true);
-
-  pinElem.style.left = data.location.x + '%';
-  pinElem.style.top = data.location.y + 'px';
-
+  pinElem.style.left = (data.location.x - AVATAR_WIDTH / 2) + 'px';
+  pinElem.style.top = (data.location.y - AVATAR_HEIGHT) + 'px';
   var pinImgElem = pinElem.querySelector('img');
   pinImgElem.src = data.author.avatar;
   pinImgElem.alt = data.offer.title;
-
   return pinElem;
+
 };
 
 var renderPins = function (data) {
@@ -76,6 +86,6 @@ var renderPins = function (data) {
   }
   return result;
 };
-
 var pinContainerElem = mapElem.querySelector('.map__pins');
 pinContainerElem.appendChild(renderPins(getOffers()));
+
