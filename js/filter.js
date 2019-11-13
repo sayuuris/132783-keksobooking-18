c50471caaad28d671f7566f8f918bb5c6f2f07bc
sayuris <oleq.artyomov@gmail.com>
2019-11-13 22:28:34 +0400

    добавляет фильр гостей

--- js/filter.js
+++ js/filter.js
@@ -4,12 +4,17 @@
   var filterType = filter.querySelector('#housing-type');
   /* var filterPrice = filter.querySelector('#housing-price'); */
   var filterRoom = filter.querySelector('#housing-rooms');
-  /* var filterGuest = filter.querySelector('#housing-guests'); */
+  var filterGuest = filter.querySelector('#housing-guests');
 
 
   var filterByType = function (offerType) {
     return filterType.value === 'any' || offerType === filterType.value;
   };
+  var filterByGuests = function (offerGuest) {
+    var offerGuestValue = filterGuest.value === 'any' ? 'any' : parseInt(filterGuest.value, 10);
+    return offerGuestValue === 'any' || offerGuest === offerGuestValue;
+  };
+
   var filterByRooms = function (offerRoom) {
     var offerRoomValue = filterRoom.value === 'any' ? 'any' : parseInt(filterRoom.value, 10);
     return offerRoomValue === 'any' || offerRoom === offerRoomValue;
@@ -17,7 +22,7 @@
 
   var filterOffers = function (offersData) {
     return offersData.filter(function (item) {
-      return item.offer && filterByType(item.offer.type) && filterByRooms(item.offer.rooms);
+      return item.offer && filterByType(item.offer.type) && filterByRooms(item.offer.rooms) && filterByGuests(item.offer.guests);
     });
   };
 
