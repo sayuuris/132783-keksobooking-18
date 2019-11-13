cb60c6720a08075521586979e57a9ea2f62e19cf
sayuris <oleq.artyomov@gmail.com>
2019-11-13 22:21:10 +0400

    фильтр дома

--- js/filter.js
+++ js/filter.js
@@ -2,13 +2,22 @@
 (function () {
   var filter = document.querySelector('.map__filters');
   var filterType = filter.querySelector('#housing-type');
+  /* var filterPrice = filter.querySelector('#housing-price'); */
+  var filterRoom = filter.querySelector('#housing-rooms');
+  /* var filterGuest = filter.querySelector('#housing-guests'); */
+
 
   var filterByType = function (offerType) {
     return filterType.value === 'any' || offerType === filterType.value;
   };
+  var filterByRooms = function (offerRoom) {
+    var offerRoomValue = filterRoom.value === 'any' ? 'any' : parseInt(filterRoom.value, 10);
+    return offerRoomValue === 'any' || offerRoom === offerRoomValue;
+  };
+
   var filterOffers = function (offersData) {
     return offersData.filter(function (item) {
-      return item.offer && filterByType(item.offer.type);
+      return item.offer && filterByType(item.offer.type) && filterByRooms(item.offer.rooms);
     });
   };
 
